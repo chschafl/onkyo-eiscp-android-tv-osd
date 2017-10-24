f8d3f627292079755f8a11859ed6d27cd8c5681f
Christian Schafleitner <christian@cs1.at>
2017-10-23 22:05:17 -0700

    Clean up

--- OnkyoVolumeToast/app/src/main/java/com/sheeply/onkyovolumetoast/SetupActivity.java
+++ OnkyoVolumeToast/app/src/main/java/com/sheeply/onkyovolumetoast/SetupActivity.java
@@ -5,11 +5,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.ToggleButton;
 
 public class SetupActivity extends AppCompatActivity {
